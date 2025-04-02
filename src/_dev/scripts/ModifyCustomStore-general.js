97e5ce362e57f5311b5ed1932cb69aafdbb1e284
ViitoJooj <joaovitor819oqures@gmail.com>
2025-04-02 15:27:00 -0300

    Lançando no github o teste

--- src/_dev/scripts/ModifyCustomStore-general.js
+++ src/_dev/scripts/ModifyCustomStore-general.js
@@ -27,5 +27,38 @@ window.lazyFunctions = {
         document.querySelectorAll('.e-lazy-script').forEach((script) => {
             script.src = script.getAttribute('data-src');
         });
-    },
+    }
 };
+
+$(document).ready(function() {
+    $('.e-main-banner__banner-list.e-desktop').slick({
+        dots: true,
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        prevArrow: '<button class="slick-prev custom-prev"><</button>',
+        nextArrow: '<button class="slick-next custom-next">></button>'
+    });
+
+    $('.product-carousel').slick({
+        infinite: false,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: true,
+        dots: true,
+        autoplay: false,
+        prevArrow: '<button class="slick-prev custom-prev"><</button>',
+        nextArrow: '<button class="slick-next custom-next">></button>',
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
+      
+      
+});
